54a6c2ca956d48df3ba3ed6e3acf4c1de24573cf
WnSg <wolfran.silva@gmail.com>
2020-05-18 22:47:02 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,17 @@ var Calculadora = {
 
 visor: document.getElementById("display"),
 valorVisor: "0",
+operacion: "",
+Valor1: 0,
+Valor2: 0,
+UltimoValor: 0,
+Resultado: 0,
+TeclaIgual: false,
 
 init: (function(){
     this.SetFormatoBotones(".tecla")
 		this.MetodosTeclasNumericas();
+    this.MetodosTeclasOperadores();
 }),
 
 // Metodos para personalizar formato y efectos de botones
@@ -56,21 +63,19 @@ AumentaBoton: function(elemento){
 
 // Metodos Get Valores de teclas Numericas
 MetodosTeclasNumericas: function(){
-  document.getElementById('0').addEventListener("click", function(){Calculadora.ingresoNumero("0");});
-  document.getElementById('1').addEventListener("click", function(){Calculadora.ingresoNumero("1");});
-  document.getElementById('2').addEventListener("click", function(){Calculadora.ingresoNumero("2");});
-  document.getElementById('3').addEventListener("click", function(){Calculadora.ingresoNumero("3");});
-  document.getElementById('4').addEventListener("click", function(){Calculadora.ingresoNumero("4");});
-  document.getElementById('5').addEventListener("click", function(){Calculadora.ingresoNumero("5");});
-  document.getElementById('6').addEventListener("click", function(){Calculadora.ingresoNumero("6");});
-  document.getElementById('7').addEventListener("click", function(){Calculadora.ingresoNumero("7");});
-  document.getElementById('8').addEventListener("click", function(){Calculadora.ingresoNumero("8");});
-  document.getElementById('9').addEventListener("click", function(){Calculadora.ingresoNumero("9");});
+  document.getElementById('0').addEventListener("click", function(){Calculadora.ClicNumero("0");});
+  document.getElementById('1').addEventListener("click", function(){Calculadora.ClicNumero("1");});
+  document.getElementById('2').addEventListener("click", function(){Calculadora.ClicNumero("2");});
+  document.getElementById('3').addEventListener("click", function(){Calculadora.ClicNumero("3");});
+  document.getElementById('4').addEventListener("click", function(){Calculadora.ClicNumero("4");});
+  document.getElementById('5').addEventListener("click", function(){Calculadora.ClicNumero("5");});
+  document.getElementById('6').addEventListener("click", function(){Calculadora.ClicNumero("6");});
+  document.getElementById('7').addEventListener("click", function(){Calculadora.ClicNumero("7");});
+  document.getElementById('8').addEventListener("click", function(){Calculadora.ClicNumero("8");});
+  document.getElementById('9').addEventListener("click", function(){Calculadora.ClicNumero("9");});
 },
 
-
-
-ingresoNumero: function(valor){
+ClicNumero: function(valor){
 		if (this.valorVisor.length < 8) {
 
 			if (this.valorVisor=="0") {
@@ -82,6 +87,107 @@ ingresoNumero: function(valor){
 		this.ActualizarVisor();
 		}
 },
+
+// Metodos Get Teclas operadores
+MetodosTeclasOperadores: function(){
+  document.getElementById('on').addEventListener("click", function() {Calculadora.LimpiarVisor();});
+	document.getElementById('sign').addEventListener("click", function() {Calculadora.CambiarSigno();});
+	document.getElementById('punto').addEventListener("click", function() {Calculadora.NumeroDecimal();});
+	document.getElementById('igual').addEventListener("click", function() {Calculadora.VerResultado();});
+	document.getElementById('raiz').addEventListener("click", function() {Calculadora.ClicOperacion("raiz");});
+	document.getElementById('dividido').addEventListener("click", function() {Calculadora.ClicOperacion("/");});
+	document.getElementById('por').addEventListener("click", function() {Calculadora.ClicOperacion("*");});
+	document.getElementById('menos').addEventListener("click", function() {Calculadora.ClicOperacion("-");});
+	document.getElementById('mas').addEventListener("click", function() {Calculadora.ClicOperacion("+");});
+},
+
+LimpiarVisor: function() {
+    this.valorVisor = "0";
+		this.operacion = "";
+		this.Valor1 = 0;
+		this.Valor2 = 0;
+		this.Resultado = 0;
+		this.Oper = "";
+		this.TeclaIgual = false;
+		this.UltimoValor = 0;
+		this.ActualizarVisor();
+},
+
+CambiarSigno: function(){
+		if (this.valorVisor !="0") {
+			var aux;
+			if (this.valorVisor.charAt(0)=="-") {
+				aux = this.valorVisor.slice(1);
+			}	else {
+				aux = "-" + this.valorVisor;
+			}
+		this.valorVisor = "";
+		this.valorVisor = aux;
+		this.ActualizarVisor();
+		}
+},
+
+NumeroDecimal: function(){
+		if (this.valorVisor.indexOf(".")== -1) {
+			if (this.valorVisor == ""){
+				this.valorVisor = this.valorVisor + "0.";
+			} else {
+				this.valorVisor = this.valorVisor + ".";
+			}
+			this.ActualizarVisor();
+		}
+},
+
+ClicOperacion: function(oper){
+		this.Valor1 = parseFloat(this.valorVisor);
+		this.valorVisor = "";
+		this.operacion = oper;
+		this.TeclaIgual = false;
+		this.ActualizarVisor();
+},
+
+VerResultado: function(){
+		if(!this.TeclaIgual){
+			this.Valor2 = parseFloat(this.valorVisor);
+			this.UltimoValor = this.Valor2;
+			this.realizarOperacion(this.Valor1, this.Valor2, this.operacion);
+
+		} else {
+			this.realizarOperacion(this.primerValor, this.ultimoValor, this.operacion);
+		}
+
+		this.Valor1 = this.Resultado;
+		this.valorVisor = "";
+
+		if (this.Resultado.toString().length < 9){
+			this.valorVisor = this.Resultado.toString();
+		} else {
+			this.valorVisor = this.Resultado.toString().slice(0,8) + "...";
+		}
+
+		this.TeclaIgual = true;
+		this.ActualizarVisor();
+},
+
+realizarOperacion: function(Valor1, Valor2, operacion){
+		switch(operacion){
+			case "+":
+				this.Resultado = eval(Valor1 + Valor2);
+			break;
+			case "-":
+				this.Resultado = eval(Valor1 - Valor2);
+			break;
+			case "*":
+				this.Resultado = eval(Valor1 * Valor2);
+			break;
+			case "/":
+				this.Resultado = eval(Valor1 / Valor2);
+			break;
+			case "raiz":
+				this.Resultado = eval(Math.sqrt(Valor1));
+		}
+	},
+
 
 ActualizarVisor: function(){
 		this.visor.innerHTML = this.valorVisor;
